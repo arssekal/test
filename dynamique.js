dca14f1f7d05dd276f265c395128ad2497ffb29e
arssekal <arssekall@gmail.com>
2024-12-02 14:49:31 +0100

    ajout d'un button vers le haut

--- dynamique.js
+++ dynamique.js
@@ -4,3 +4,22 @@ const links = document.querySelector(".links");
 navToggle.addEventListener("click", function () {
   links.classList.toggle("show-links");
 });
+
+// code concernant le button vers le haut
+// Sélectionner le bouton "Retour en haut"
+let backToTopButton = document.getElementById("backToTopBtn");
+
+// Afficher le bouton lorsque l'utilisateur fait défiler la page de plus de 500 pixels
+window.onscroll = function() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        backToTopButton.style.display = "block";  // Afficher le bouton
+    } else {
+        backToTopButton.style.display = "none";  // Cacher le bouton
+    }
+};
+
+// Lorsque le bouton est cliqué, faire défiler la page vers le haut
+backToTopButton.onclick = function() {
+    document.body.scrollTop = 0;  // Pour les navigateurs Webkit (Safari)
+    document.documentElement.scrollTop = 0;  // Pour les autres navigateurs
+};
